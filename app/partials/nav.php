67ddb1aa6c9c076243579aa13b0c763177e2c4a2
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-03 18:24:52 +0200

    nav update

--- app/partials/nav.php
+++ app/partials/nav.php
@@ -1,3 +1,21 @@
-<nav>
-
+<nav class="nav_d">
+    <div class="nav_d_container">
+        <div class="cart">
+            <i></i>
+            <p>Kundvagn - 0</p>
+        </div>
+        <div class="nav_d_logo">
+            <a href="">
+                <img src="" alt="">
+            </a>
+        </div>
+        <div class="nav_d_link">
+            <a href="">Hem</a>
+            <a href="">Om</a>
+            <a href="">Prudukter</a>
+            <a href="">Media</a>
+            <a href=""></a>
+            <a href="">Kontakta</a>
+        </div>
+    </div>
 </nav>
