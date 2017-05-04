8a7888e75ca3a318cf249b9bf0222654fc191267
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-04 18:53:55 +0200

    nav update

--- app/index.php
+++ app/index.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="orgel, instrument, musik"/>
 </head>
 
-<body class="wrapper col-xs-12" id="page-top" class="index">
+<body class="wrapper col-xs-10" id="page-top" class="index">
 <?php include "partials/nav.php"?>
 
 <header class="container-fluid i_header">
@@ -24,9 +24,9 @@
 
 <section class="i_info container-fluid">
     <div class="row-fluid i_info">
-        <div class="col-xs-12 i_info_container col-xs-8 col-xs-offset-2">
+        <div class="col-xs-12 i_info_container col-xs-offset-2">
             <h2>Vad vi gör</h2>
-            <p>Herp derpsum berp me berps merp der ler derperker derp.
+            <p> Herp derpsum berp me berps merp der ler derperker derp.
                 Sherp pee terp herderder derperker?
                 Ner dee terp serp derp merpus.
                 Sherper herpsum derpy sherpus herpderpsmer
@@ -34,7 +34,8 @@
                 Pee herpy tee dee, berp ter sherper me herp.
                 Perper herpem perp derpler le, me tee mer.
                 Tee berps nerpy derperker merp me.
-                Serp herp derp mer berps der berp merpus!</p>
+                Serp herp derp mer berps der berp merpus!
+            </p>
         </div>
     </div>
 </section>
--- app/partials/nav.php
+++ app/partials/nav.php
@@ -1,21 +1,27 @@
-<nav class="nav_d">
+<nav class="nav_d col-xs-2">
     <div class="nav_d_container">
+
+        <!-- cart icon -->
         <div class="cart">
-            <i></i>
+            <i class="fa fa-shopping-cart"></i>
             <p>Kundvagn - 0</p>
         </div>
+
+        <!-- Logo -->
         <div class="nav_d_logo">
             <a href="">
                 <img src="" alt="">
             </a>
         </div>
+
+        <!--Nav links-->
         <div class="nav_d_link">
-            <a href="">Hem</a>
-            <a href="">Om</a>
-            <a href="">Prudukter</a>
-            <a href="">Media</a>
-            <a href=""></a>
-            <a href="">Kontakta</a>
+            <a href="#">Hem</a>
+            <a href="#">Om</a>
+            <a href="#">Prudukter</a>
+            <a href="#">Media</a>
+            <a href="#">Kontakta</a>
         </div>
+
     </div>
 </nav>
