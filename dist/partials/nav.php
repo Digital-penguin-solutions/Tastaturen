0106f2e525bf5b5d694732238c04a43219db8e6e
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-07 19:08:14 +0200

    test to develop project

--- dist/partials/nav.php
+++ dist/partials/nav.php
@@ -1,1 +1,1 @@
-<nav></nav>
+<nav class="nav_d col-xs-2"><div class="nav_d_container"><!-- cart icon --><div class="cart"><a href="#"><i class="fa fa-bell-o"></i> Kontakta för köp</a></div><!-- Logo --><div class="nav_d_logo"><a href="#"><img src="" alt=""></a></div><!--Nav links--><div class="nav_d_link"><a href="#">Hem</a> <a href="#">Om oss</a> <a href="#">orglar för hemet</a> <a href="#">orglar för kyrkan </a><a href="#">Media</a> <a href="#">Kontakta oss</a></div></div></nav>
