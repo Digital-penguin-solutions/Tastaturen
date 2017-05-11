c5e06df1a55035f8bebf0677c2e5ffd1f7b8ae5b
peduncle <jakob.frykmer@gmail.com>
2017-05-11 15:58:15 +0200

    prepare admin stuff

---
+++ app/admin.php
@@ -0,0 +1,143 @@
+<?php
+include "include_pages/loading.php";
+include "include_pages/head.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="description" content="MetSense Admin page. the page to edit the pruduckts and the content of the website">
+    <title>Admin page</title>
+</head>
+
+<?php
+include "function/functions.php";
+session_start();
+$con = connect();
+$products = get_all_products($con);
+
+?>
+<body>
+<?php include "include_pages/nav.php" ?>
+
+<section class = "admin_page">
+    <div class = "container-fluid full_height">
+        <div class = "row full_height">
+            <div class = "col-md-8 col-md-offset-2">
+                <h1 class = "admin_header"> Admin page </h1>
+
+                <?php
+                if(isset($_GET['wrong'])){
+                    echo "<h2 class = 'admin_header'> Wrong password, please try again </h2>"; 
+                }
+
+                if(isset($_SESSION['admin'])){
+
+                    if(isset($_GET['logout'])){
+                        session_destroy();
+                        header("Location: admin.php");
+                    }
+                    if(isset($_GET['message'])){
+                        $message = $_GET['message'];
+                        echo "<h1 class = 'admin_message'>" . $message . "</h1>";
+
+                    }
+
+                    if(isset($_GET['change_password'])){
+
+                        ?>
+                        <form class="login change_password_form" action = "function/login.php" method = "post">
+                            <p>New password:</p>
+                            <input placeholder = "New password" type = "password" name = "password"><br>
+                            <input placeholder = "Repeat new password" type = "password" name = "password_repeat">
+                            <input type = "submit" name = "set_password" value="Change">
+                        </form>
+                        <?php
+                    }
+                    ?>
+                    <div class = "row admin_all_products_container">
+
+                        <?php
+                        $count = 0;
+                        foreach ($products as $product) {
+                            $name       = $product['name'];
+                            $main_image = $product['main_image'];
+                            $product_id = $product['product_id'];
+                            $show       = $product['show'];
+
+
+                            if($show == 1){
+                                $toggle_button_value = "Hide product";
+                                $toggle_color        = "red";
+                            }
+                            else {
+                                $toggle_button_value = "Set visible";
+                                $toggle_color        = "green";
+                            }
+
+
+                            if($count % 2 == 0) {
+                                $offset = 1;
+                            }
+                            else {
+                                $offset = 2;
+                            }
+                            ?>
+                            <div class = "col-md-4 col-md-offset-<?php echo $offset ?> admin_product">
+                                <h1><a href = "product?p=<?php echo $name?>"><?php echo $name ?></a></h1>
+                                <img class = "center_horizontally_css" src="data:image/jpeg;base64,<?php echo base64_encode( $main_image ); ?>" alt="the prudukt that you whant to edit"/>
+
+                                <!--- EDIT BUTTON-->
+                                <a href = "add_product?product_id=<?php echo $product_id?>" class = "product_button product_edit_button">
+                                    <p class = "center_vertically_css">Edit</p>
+                                </a>
+
+                                <!--- TOGGLE SHOW BUTTON-->
+                                <a href = "function/toggle_product?product_id=<?php echo $product_id?>" class = "product_button <?php echo $toggle_color?> product_show_button">
+                                    <p class = "center_vertically_css"><?php echo $toggle_button_value?></p>
+                                </a>
+
+                                <!--- DELETE BUTTON-->
+                                <a href = "function/delete_product?id=<?php echo $product_id?>" class = "product_button product_delete_button">
+                                    <p class = "center_vertically_css">Delete</p>
+                                </a>
+
+                            </div>
+                            <?php
+                            $count++;
+                        }
+                        ?>
+                    </div>
+                    <?php
+
+                }
+
+                if(isset($_SESSION['admin'])){
+                    ?>
+                    <a href = "add_product" class = "add_product_button center_horizontally_css">
+                        Add a new product
+                    </a>
+                    <a href = "admin?change_password=" class = "add_product_button center_horizontally_css">
+                        Change password
+                    </a>
+                    <a href = "admin?logout=" class = "add_product_button center_horizontally_css">
+                        Logout
+                    </a>
+                    <?php
+                }
+                else {
+                    ?>
+                    <form class="login hidden-sm hidden-xs" action = "function/login.php" method = "post">
+                        <p>PASSWORD:</p>
+                        <input type = "password" name = "password">
+                        <input type = "submit" name = "login" value="Login">
+                    </form>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
---
+++ app/functions/functions.php
@@ -0,0 +1,307 @@
+<?php
+//all functions for Metsense.com
+
+//check if functions is included in page to acses them
+if(!isset($functions_included)){
+
+    $functions_included = true;
+
+    //include connect page
+    include "db_connect.php";
+
+    // secures a string, protects against SQL injections and other attacks
+    function secure_str($data){
+        $con = connect();
+        //$data = stripslashes($data);
+        $data = trim($data);
+        $data = addslashes($data);
+        $data = mysqli_real_escape_string($con, $data);
+        $data = strip_tags($data);
+
+        return $data;
+    }
+
+    //read slider images
+    function read_slider_images($con, $index){
+
+        global $slider_images_array;
+
+        if(isset($_POST[$index])) {
+
+            $images = $_POST[$index];
+
+            for ($i = 0; $i < sizeof($images); $i++){
+
+                $data     = $images[$i];
+                $split    = explode("@)(@#!#!#", $data); // splits the data string into image_data, filename and image_id
+
+                $data     = $split[0]; // image data is one the first index
+                $filename = $split[1];
+                $image_id = $split[2];
+
+                if ($data) {
+
+                    $data = preg_replace('/^data:image\/(png|jpg|jpeg);base64,/', '', $data);
+                    $data = mysqli_real_escape_string($con, base64_decode($data));
+
+                    $info = array($data, $filename, $image_id);
+                    $slider_images_array[] = $info;
+                }
+                else {
+                }
+            }
+        }
+        else {
+            echo "not found";
+        }
+
+    }
+
+    //read other images
+    function read_image($con, $index){
+
+        $result;
+        global $images_array;
+
+        if(isset($_POST[$index])) {
+
+            $data  = $_POST[$index];
+
+            $split = explode("@)(@#!#!#", $data); // splits the data string into image_data, filename and image_id
+
+            $data  = $split[0]; // image data is one the first index
+
+            if ($data) {
+
+                $data = preg_replace('/^data:image\/(png|jpg|jpeg);base64,/', '', $data);
+                $data = mysqli_real_escape_string($con, base64_decode($data));
+                //$image = file_put_contents($image_data);
+
+
+                $result = $data;
+
+                $info           = array($data, $index);
+                $images_array[] = $info;
+            }
+            else {
+                $result = "empty";
+            }
+        }
+        else {
+            echo "not found single <br>";
+        }
+
+    }
+
+    /* Return only the products that are to be shown on the homepage */
+    function get_all_visible_products($con) {
+        $query  = "SELECT product_id, name, short_description, main_image FROM product WHERE `show` = '1'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+
+        $array  = array();
+
+        while($data = mysqli_fetch_array($select)){
+
+            $array[] = $data;
+        }
+
+
+        return $array;
+
+    }
+    //get all products from database
+    function get_all_products($con) {
+
+        $query  = "SELECT * FROM product";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+
+        $array  = array();
+
+        while($data = mysqli_fetch_array($select)){
+
+            $array[] = $data;
+        }
+
+
+        return $array;
+    }
+
+    //get all pruduckt from the database by id
+    function get_product_by_id($con, $id){
+
+        $id = secure_str($id);
+        $query = "SELECT * FROM product WHERE product_id = '$id'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+        $data = mysqli_fetch_array($select);
+
+        return $data;
+    }
+
+    //get long description by id from database
+    function get_product_long_by_id($con, $id){
+
+        $id = secure_str($id);
+        $query = "SELECT long_description FROM product WHERE product_id = '$id'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+        $data = mysqli_fetch_array($select);
+
+        $long = $data['long_description'];
+
+        return $long;
+    }
+
+    // the tech table is stored as a large string with '#!' seperating each row and '%!' seperating each part of a row
+    function get_tech_table_by_id($con, $id){
+        $id = secure_str($id);
+        $query = "SELECT * FROM tech_table_row WHERE product_id = '$id' ORDER BY tech_table_row_id";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+
+        $array = array();
+
+
+        while ($data = mysqli_fetch_array($select)) {
+            $left      = $data["left_content"];
+            $right     = $data["right_content"];
+
+            $array_tmp = array($left, $right);
+
+            $array[]   = $array_tmp;
+        }
+
+        return $array;
+    }
+
+    function get_product_visibility_by_id($con, $id){
+        $id    = secure_str($id);
+        $data = get_product_by_id($con, $id);
+
+        return $data['show'];
+    }
+
+    //get product by name from database
+    function get_product_id_by_name($con, $name){
+
+        $id     = secure_str($name);
+        $query  = "SELECT * FROM product WHERE name = '$name'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+
+
+        $data   = mysqli_fetch_array($select);
+        $id     = $data['product_id'];
+
+        return $id;
+
+    }
+
+    // gets all slider images
+    function get_product_images_by_id($con, $id){
+
+        $id     = secure_str($id);
+        $query  = "SELECT * FROM product_image WHERE product_id = '$id'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+
+        $array  = array();
+
+        while ($data = mysqli_fetch_array($select)) {
+            $array[] = $data;
+        }
+
+        return $array;
+
+    }
+
+    //get name of prudukt by id from database
+    function get_product_name_by_id($con, $id){
+        $id     = secure_str($id);
+        $query  = "SELECT name FROM product WHERE product_id = '$id'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+        $data   = mysqli_fetch_array($select);
+
+        $name = $data['name'];
+        return $name;
+    }
+
+    //get short description by id from database
+    function get_product_short_by_id($con, $id){
+        $id          = secure_str($id);
+        $query       = "SELECT short_description FROM product WHERE product_id = '$id'";
+        $select      = mysqli_query($con, $query) or die (mysqli_error($con));
+        $data        = mysqli_fetch_array($select);
+
+        $description = $data['short_description'];
+        return $description;
+    }
+
+    //get price from database by id
+    function get_product_price_by_id($con, $id){
+        $id     = secure_str($id);
+        $query  = "SELECT price FROM product WHERE product_id = '$id'";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+        $data   = mysqli_fetch_array($select);
+
+        $price = $data['price'];
+        return $price;
+    }
+
+    //get key featuers by id from database
+    function get_key_features_by_id($con, $id){
+        $id     = secure_str($id);
+        $query  = "SELECT * FROM key_feature WHERE product_id = '$id' ORDER BY key_feature_id";
+        $select = mysqli_query($con, $query) or die (mysqli_error($con));
+        $array  = array();
+
+        while ($data = mysqli_fetch_array($select)) {
+            $array[] = $data["content"];
+        }
+
+        return $array;
+    }
+
+    //logout from admin page
+    function logout(){
+        session_start();
+        session_destroy();
+    }
+
+    //compress images
+    function compress($source, $destination, $quality) {
+        $info = getimagesize($source);
+
+        if ($info['mime']     == 'image/jpeg') {
+            $image = imagecreatefromjpeg($source);
+        }
+        elseif ($info['mime'] == 'image/gif'){
+            $image = imagecreatefromgif($source);
+        }
+        elseif ($info['mime'] == 'image/png') {
+            $image = imagecreatefrompng($source);
+        }
+        imagejpeg($image, $destination, $quality);
+        return $destination;
+    }
+
+    function return_bytes($val) {
+        $val = trim($val);
+        $last = strtolower($val[strlen($val)-1]);
+        switch($last) {
+            // The 'G' modifier is available since PHP 5.1.0
+            case 'g':$val *= 1024;
+            case 'm':$val *= 1024;
+            case 'k':$val *= 1024;
+        }
+
+        return $val;
+    }
+
+    //debugging website in consol
+    function debug_to_console( $data ) {
+
+        if ( is_array( $data ) )
+            $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+        else
+            $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+        echo $output;
+    }
+}
+?>
