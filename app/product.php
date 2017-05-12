b680367cddb185aa66783951d2ec5116a16025b1
peduncle <jakob.frykmer@gmail.com>
2017-05-12 18:32:12 +0200

    felstavning fixad

---
+++ app/product.php
@@ -0,0 +1,49 @@
+<?php include "partials/head.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>prudukter</title>
+    <meta name="description" content="Tastaturen"/>
+    <meta name="keywords" content="orgel, instrument, musik"/>
+</head>
+
+<body class="wrapper col-xs-12 col-md-10" id="page-top" class="index">
+<?php include "partials/nav.php"?>
+
+<header class="container-fluid p_prod_head">
+    <div class="row-fluid p_prod_head">
+        <div class="col-xs-12 p_prod_head_container">
+            <div class="p_prod_head_bg">
+                <div class="p_prod_head_text">
+                    <h1>Prudukt namn</h1>
+                    <h2>Modell saker</h2>
+                </div>
+                <div class="p_prod_head_btn_broschyr">
+                    <button>Ladda ner Broschyr</button>
+                </div>
+                <div class="p_prod_head_btn_order">
+                    <button>Skicka en offert</button>
+                </div>
+                <div class="p_prod_head_img">
+                    <img src="img/product/kyrka/rembrant_350.png" alt="produkt bild">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="container-fluid p_info">
+    <div class="row-fluid p_info">
+        <div class="col-xs-12 p_info_container">
+
+
+
+        </div>
+    </div>
+</section>
+
+
+
+<?php include "partials/footer.php" ?>
+</body>
+</html>
