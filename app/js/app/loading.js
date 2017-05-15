240020ddc21cfb569035852e07f735d90bd53b8e
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-15 13:57:34 +0200

    implementera en loading screen. Förstörde index slider

--- app/js/app/loading.js
+++ app/js/app/loading.js
@@ -1,1 +1,17 @@
-$(document).ready(on_ready);
+$(document).ready(function() {
+
+    var counter = 0;
+    var c = 0;
+    var i = setInterval(function(){
+        $(".loading-page .counter h1").html(c + "%");
+        $(".loading-page .counter hr").css("width", c + "%");
+
+        counter++;
+        c++;
+
+        if(counter == 101) {
+            clearInterval(i);
+            document.body.removeChild(load_screen);
+        }
+    }, 50);
+});
---
+++ app/partials/loading.php
@@ -0,0 +1,7 @@
+<div class="loading-page" id="load_screen">
+    <div class="counter">
+        <p>Loading</p>
+        <h1>0%</h1>
+        <hr>
+    </div>
+</div>
