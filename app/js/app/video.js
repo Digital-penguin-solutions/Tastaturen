8be5ac24a98edaf554125084418c4419d75b80d3
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-05-29 01:29:01 +0200

    video player update

--- app/js/app/video.js
+++ app/js/app/video.js
@@ -2,12 +2,13 @@ function onYouTubeIframeAPIReady() {
     var player;
     player = new YT.Player('organvideo', {
         videoId: 'z8kBoDdQOgc', // YouTube Video ID
+        playlist:'z8kBoDdQOgc',
         playerVars: {
             autoplay: 1,        // Auto-play the video on load
-            controls: 0,        // Show pause/play buttons in player
+            controls: 1,        // Show pause/play buttons in player
             showinfo: 0,        // Hide the video title
             modestbranding: 1,  // Hide the Youtube Logo
-            loop: 1,            // Run the video in a loop
+            loop: 2,            // Run the video in a loop
             fs: 0,              // Hide the full screen button
             cc_load_policy: 0,  // Hide closed captions
             iv_load_policy: 3,  // Hide the Video Annotations
