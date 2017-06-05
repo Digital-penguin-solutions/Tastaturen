24ddf823271f7aec3eb83e6b098528280b2a08f2
peduncle <jakob.frykmer@gmail.com>
2017-06-05 17:55:35 +0200

    admin stuff

--- app/views/admin_info.php
+++ app/views/admin_info.php
@@ -1,14 +1,16 @@
 <?php
     
-session_start();
+if(!isset($no_admin_info)){
+    session_start();
 
-if(isset($_SESSION['admin'])){
-?>
-    <div class = "admin_info_container">
-        <p>You are currently logged in as admin. You can click on most textfields to edit them.</p>
-        <p>To edit products, use the <a href = 'admin'>admin page</a></p>
-    </div>
-<?php
+    if(isset($_SESSION['admin'])){
+    ?>
+        <div class = "admin_info_container">
+            <p>You are currently logged in as admin. You can click on most textfields to edit them.</p>
+            <p>To edit products, use the <a href = 'admin'>admin page</a></p>
+        </div>
+    <?php
+    }
 }
 
 ?>
