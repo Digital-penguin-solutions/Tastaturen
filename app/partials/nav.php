6a4e04c36a3e58c7bc8bc9c6b6d1956bab10f8a7
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-06-10 16:29:24 +0200

    la till toggle knappen på mobil och tablet. fungerar dock inte

--- app/partials/nav.php
+++ app/partials/nav.php
@@ -1,7 +1,5 @@
 <nav class="nav_d col-md-2 col-xs-7" role="navigation">
 
-    <div class="nav_d_btn hidden-md hidden-lg"></div>
-
     <div class="nav_d_container">
 
         <div class="nav_d_triage"></div>
@@ -32,3 +30,15 @@
         </div>
     </div>
 </nav>
+
+<div class="nav_d_btn">
+    <ul class = "nav_button_container">
+        <li>
+            <a class = "McButton" data = "hamburger-menu">
+                <b></b>
+                <b></b>
+                <b></b>
+            </a>
+        </li>
+    </ul>
+</div>
