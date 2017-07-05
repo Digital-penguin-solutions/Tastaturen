9aef77ccf22f8e056cf1103294be25f42e7770ab
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2017-07-05 22:56:39 +0200

    remove uppercase stuff and loading css

--- app/views/offert.php
+++ app/views/offert.php
@@ -15,7 +15,7 @@
 
             <p class="col-xs-offset-2 col-xs-4">Message</p>
             <textarea type="text" name="message" id="message" placeholder="Optional message" class="col-xs-8 col-xs-offset-2"></textarea>
-            <button type="button" class="offert_send col-xs-4 col-xs-offset-4">bäställ en ofärt</button>
+            <button type="button" class="offert_send col-xs-4 col-xs-offset-4">Begär en offert</button>
         </form>
 
         <div class="offert_product col-md-6 hidden-xs">
