b94efb7b66799ab5febad9341340ebbb99104003
peduncle <jakob.frykmer@gmail.com>
2018-01-16 17:06:28 +0100

    s

--- app/functions/db_connect.php
+++ app/functions/db_connect.php
@@ -1,18 +1,28 @@
 <?php
 /**
-  * Coneckting the the database
-  * 
-  * Conecting to the database
-  *
-  * @param author Digitalis
-*/
+ * Coneckting the the database
+ *
+ * Conecting to the database
+ *
+ * @param author Digitalis
+ */
+
 function connect(){
-    //$con = mysqli_connect("treelineweb.com", "fcclftva_user", "daggmask", "fcclftva_f");
-    $con = mysqli_connect("146.185.150.217", "user2", "UCIzQyGU61iQv1Z3", "tastaturen");
-    if (mysqli_connect_errno()){
-        echo "Failed". mysqli_connect_error();
+    // Define connection as a static variable, to avoid connecting more than once
+    static $connection;
+
+    // Try and connect to the database, if a connection has not been established yet
+    if(!isset($connection)) {
+        // Load configuration as an array. Use the actual location of your configuration file
+        $config = parse_ini_file('../config.ini');
+        $connection = mysqli_connect($config['host'],$config['username'],$config['password'],$config['dbname']);
     }
-    mysqli_set_charset($con, "utf-8");
-    return $con;
+    if($connection === false) {
+        die("Connection failed: " . mysqli_connect_error());
+        header("Location: 404");
+
+    }
+    mysqli_set_charset($connection, "utf-8");
+    return $connection;
 }
 ?>
