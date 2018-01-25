1ae29f8fda9b7a6df5af0b25816a1d274738b61d
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2018-01-25 12:56:01 +0100

    event page is done designing

--- Notes.md
+++ Notes.md
@@ -4,14 +4,13 @@
 -[ ] Byta första bilden. En orgel istället för två `index`. 
 -[ ] När man laddar ner broschyr på iPhone så försöker den förhandsgranska den.
 -[ ] php till sidan `events`.
--[ ] landskap för padda
 
 ## Done
+-[x] landskap för padda
 -[x] Produkt sidan funkar inte så bra för mobil. (när man är inne på en enskild produkt).
 -[x] landskap fungerar på telefon
 -[x] Gömma ladda ner brochyren knappen när det inte finns någon brochyr för enheten.
 -[x] lägga till event section som inte syns när det inte finns några event.
->>>>>>> 30cb4461a26a4193a9f85b403bbe547dd05134fe
 -[x] MEdia sakerna fungerar inte ordentligt.
 -[x] Fixa att orglarna blir konstig storlek när du inte har browser fönstret i fullskärm 
 -[x] Göra bakrunden för orglarna ljusare I hem och kyrka alternatift lägga ett filter över
--- app/views/events.php
+++ app/views/events.php
@@ -2,9 +2,52 @@
     <div class="i_event row">
         <div class="i_event_container col-xs-12">
             <h1><?php print_field("i_event_header"); ?></h1>
-            <div class="i_event_container_text">
+            <div class="i_event_event_container col-xs-12">
+
+                <div class="i_event_img col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+                    <img src="img/event/test.jpg" alt="Tastaturen tevent">
+                </div>
+
+                <div class="i_event_text i_event_img col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+                    <h2 class="i_event_header">Test event</h2>
+                    <p class="i_event_date">30 Januari 2018</p>
+                    <p class="i_event_info">Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Suspendisse imperdiet finibus sem vitae auctor.
+                        Phasellus quis turpis eu lacus volutpat commodo. Sed volutpat eleifend sem.</p>
+                </div>
 
             </div>
+            <div class="i_event_event_container col-xs-12">
+
+                <div class="i_event_img col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+                    <img src="img/event/test.jpg" alt="Tastaturen tevent">
+                </div>
+
+                <div class="i_event_text i_event_img col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+                    <h2 class="i_event_header">Test event</h2>
+                    <p class="i_event_date">30 Januari 2018</p>
+                    <p class="i_event_info">Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Suspendisse imperdiet finibus sem vitae auctor.
+                        Phasellus quis turpis eu lacus volutpat commodo. Sed volutpat eleifend sem.</p>
+                </div>
+
+            </div>
+            <div class="i_event_event_container col-xs-12">
+
+                <div class="i_event_img col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+                    <img src="img/event/test.jpg" alt="Tastaturen tevent">
+                </div>
+
+                <div class="i_event_text i_event_img col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0">
+                    <h2 class="i_event_header">Test event</h2>
+                    <p class="i_event_date">30 Januari 2018</p>
+                    <p class="i_event_info">Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Suspendisse imperdiet finibus sem vitae auctor.
+                        Phasellus quis turpis eu lacus volutpat commodo. Sed volutpat eleifend sem.</p>
+                </div>
+
+            </div>
+
         </div>
     </div>
 </section>
